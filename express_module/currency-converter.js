1e77d0a298d4738db894fa051c6c86bc9fe64fef
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-03-05 15:50:21 +0800

    Update currency-converter.js

--- express_module/currency-converter.js
+++ express_module/currency-converter.js
@@ -3,10 +3,25 @@
 // countries: https://restcountries.eu/rest/v2/currency/${currencyCode}
 
 
+const axios = require('axios');  // 通过 require 语法引入 axios
+
 
 // 1st function - getExchangeRate
+const getExchangeRate = async (fromCurrency, toCurrency) => {  
+    const response = await axios.get('http://data.fixer.io/api/latest?    access_key=[yourAccessKey]&format=1');
+    const rate = response.data.rates;
+    const euro = 1 / rate[fromCurrency];
+    const exchangeRate = euro * rate[toCurrency];
+
+    return exchangeRate;
+}
 
 // 2nd function - getCountries
+const getCountries = async (currencyCode) => {
+const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
+
+return response.data.map(country => country.name);
+}
 
 // 3rd function - convertCurrency
 
