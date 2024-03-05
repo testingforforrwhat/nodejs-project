71e90392a8c31a18ab968f5381bee034cb90ffe9
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-03-05 12:53:38 +0800

    update

--- functionsModule.js
+++ functionsModule.js
@@ -1,5 +1,9 @@
 // functionsModule.js
 
+// 通过引入 fs 模块，您可以在 Node.js 环境中进行文件的读取、写入、修改等操作
+const fs = require('fs');
+
+
 function add(a, b) {
     return a + b;
 }
@@ -8,7 +12,30 @@ function subtract(a, b) {
     return a - b;
 }
 
+function readJSONFiles(files, callback) {
+    let count = files.length;
+    const results = [];
+
+    files.forEach((file) => {
+        fs.readFile(file, 'utf8', (err, data) => {
+            if (err) {
+                results.push({ file, error: err.message });
+            } else {
+                results.push({ file, content: JSON.parse(data) });
+            }
+
+            count--;
+
+            if (count === 0) {
+                callback(results);
+            }
+        });
+    });
+}
+
 module.exports = {
     add,
-    subtract
+    subtract,
+    readJSONFiles
 };
+
--- readJson.js
+++ readJson.js
@@ -1,27 +1,9 @@
 
-// 通过引入 fs 模块，您可以在 Node.js 环境中进行文件的读取、写入、修改等操作
-const fs = require('fs');
 
-function readJSONFiles(files, callback) {
-    let count = files.length;
-    const results = [];
 
-    files.forEach((file) => {
-        fs.readFile(file, 'utf8', (err, data) => {
-            if (err) {
-                results.push({ file, error: err.message });
-            } else {
-                results.push({ file, content: JSON.parse(data) });
-            }
 
-            count--;
 
-            if (count === 0) {
-                callback(results);
-            }
-        });
-    });
-}
+const { readJSONFiles } = require('./functionsModule');
 
 // 要读取的 JSON 文件列表
 const filesToRead = ['./resources/test.json', './resources/noteExist.json'];
