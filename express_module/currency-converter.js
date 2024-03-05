9a3670df8dc1d4729838c38179450c2bf4112d2e
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-03-05 15:33:26 +0800

    Update currency-converter.js

---
+++ express_module/currency-converter.js
@@ -0,0 +1,13 @@
+// api
+// exchange rate: http://data.fixer.io/api/latest?access_key=f68b13604ac8e570a00f7d8fe7f25e1b&format=1
+// countries: https://restcountries.eu/rest/v2/currency/${currencyCode}
+
+
+
+// 1st function - getExchangeRate
+
+// 2nd function - getCountries
+
+// 3rd function - convertCurrency
+
+// Call convert currency to get meaningful data.
