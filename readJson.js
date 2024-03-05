0325c0162dd273f53e4ba2e33af9ded88e0a96b2
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-03-05 13:07:00 +0800

    update

--- readJson.js
+++ readJson.js
@@ -5,6 +5,7 @@ const { readJSONFiles } = require('./functionsModule');
 // 要读取的 JSON 文件列表
 const filesToRead = ['./resources/test.json', './resources/noteExist.json'];
 
+// 阻塞是按顺序执行的，而非阻塞是不需要按顺序的
 readJSONFiles(filesToRead, (results) => {
     results.forEach((result) => {
         if (result.error) {
