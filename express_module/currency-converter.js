5941e8208609f39eb1f03891ecf65f9f1e3370af
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-03-05 17:03:24 +0800

    Update currency-converter.js

--- express_module/currency-converter.js
+++ express_module/currency-converter.js
@@ -9,7 +9,7 @@ const axios = require('axios');  // 通过 require 语法引入 axios
 // 1st function - getExchangeRate
 const getExchangeRate = async (fromCurrency, toCurrency) => {  
     try {    
-        const response = await       axios.get('http://data.fixer.io/api/latest?access_key=f68b13604ac8e570a00f7d8fe7f25e1b&format=1');    
+        const response = await axios.get('http://data.fixer.io/api/latest?access_key=f68b13604ac8e570a00f7d8fe7f25e1b&format=1');    
         const rate = response.data.rates;    
         const euro = 1 / rate[fromCurrency];    
         const exchangeRate = euro * rate[toCurrency];    
